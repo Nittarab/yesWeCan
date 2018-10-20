0d5f343d09a63666e2de0702ab09a4e9519e5a14
patrick <p.barattin@gmail.com>
2018-10-21 00:39:49 +0200

    [FIX] indentation

--- serverless_python/handler.py
+++ serverless_python/handler.py
@@ -15,11 +15,11 @@ def translate(event, context):
     intent = body_event["queryResult"]["intent"]
 
     if intent == 'getWaste':
-       body = getWaste(SESSION_ID, original, waste, status)
+        body = getWaste(SESSION_ID, original, waste, status)
     elif intent == 'getStatus':
-       body = getWaste(SESSION_ID, original, waste, status)
+        body = getWaste(SESSION_ID, original, waste, status)
     else:
-       body = generateResponse("oooh what?")
+        body = generateResponse("oooh what?")
 
     print(body)
 
@@ -28,69 +28,72 @@ def translate(event, context):
         "body": json.dumps(body)
     }
 
-  return response
+
+return response
+
 
 def getWaste(SESSION_ID, original, waste, status):
-  output_params = {"waste": waste}
+    output_params = {"waste": waste}
 
-  file = open('waste.csv', 'r')
-  dataset = [line.rstrip('\n').split(',') for line in file] 
-  file.close()
+    file = open('waste.csv', 'r')
+    dataset = [line.rstrip('\n').split(',') for line in file]
+    file.close()
 
-  wastes = []
+    wastes = []
 
-  for row in dataset:
-    if waste in [value.strip() for value in row[4].split('|')]:
-        wastes.append(row)
+    for row in dataset:
+        if waste in [value.strip() for value in row[4].split('|')]:
+            wastes.append(row)
 
+    if len(wastes) > 1:
+        message = "Do you mean " + ' or '.join('**' + value + '**' for value in wastes[3]) + "?"
+        response = generateResponse(SESSION_ID, output_params, message)
+    elif len(wastes) == 1:
 
-  if len(wastes) > 1:
-    message = "Do you mean " + ' or '.join('**' + value + '**' for value in wastes[3]) + "?"
-    response =generateResponse(SESSION_ID, output_params, message)
-  elif len(wastes) == 1:
+        if status != '':
+            if status == 'clean':
+                response = generateResponse(SESSION_ID, output_params, wastes[0][1])
+            elif status == 'dirty':
+                response = generateResponse(SESSION_ID, output_params, wastes[0][2])
+            else:
+    response = generateResponse(SESSION_ID, output_params,
+                                "Is the " + waste + " clean or dirty?")
 
-	if status != '':
-        if status = 'clean':
-            response = generateResponse(SESSION_ID, output_params, wastes[0][1]) 
-        elif status == 'dirty':
-            response = generateResponse(SESSION_ID, output_params, wastes[0][2]) 
-	else:
-       response = generateResponse(SESSION_ID, output_params,
-                                 "Is the " + waste + " clean or dirty?")
-
-  return response
+    return response
 
 
 def getStatus(body_event): pass
 
 
 def generateResponse(SESSION_ID, output_params, message):
-	body = {
-		"payload": {
-		    "google": {
-		        "expectUserResponse": True,
-		        "richResponse": {
-		            "items": [
-		                        {
-		                            "simpleResponse": {
-		                                "textToSpeech": message
+    body = {
+        "payload": {
+            "google": {
+                "expectUserResponse": True,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": message
 
-		                            }
-		                        }
-		            ]
-		        }
-		    }
-		},   "outputContexts": [
-		    {
-		        "name": "projects/testrecycling/agent/sessions/%s/contexts/context name" % SESSION_ID,
-		        "lifespanCount": 5,
-		        "parameters": {
-		            output_params
-		        }
-		    }
-		],
-		"followupEventInput": {
-		    "name": "event name",
-		    "languageCode": "en-US",
-		}
-	}
+                            }
+                        }
+                    ]
+                }
+            }
+        }, "outputContexts
+        : [
+                {
+                    "name": "pro
+                    ects / testrecycling / agent / sessions / % s / contexts / context name" % SESSION_ID,
+                                                                                           "lifespanCount": 5,
+                    "parameters": {
+                        output_params
+                    }
+                }
+            ],
+        "followupEventInput": {
+            "name": "event name",
+            "languageCode": "en-US",
+        }
+    }
