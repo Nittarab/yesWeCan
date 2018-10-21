8b4c2ed2478641dd0030ffcdbe754672516639d4
Bogdan <vasile.pop@studenti.unitn.it>
2018-10-21 02:39:42 +0200

    [FIX] some stuff

--- serverless_python/handler.py
+++ serverless_python/handler.py
@@ -4,96 +4,103 @@ from db import getBin
 
 
 def translate(event, context):
-    body_event = json.loads(event["body"])
-    print(body_event)
+	body_event = json.loads(event["body"])
 
-    parameters = body_event["queryResult"]["parameters"]
-    SESSION_ID = body_event["session"]
-    original = parameters["original"]
-    waste = parameters["waste"]
-    status = parameters["status"]
-    intent = body_event["queryResult"]["intent"]
 
-    if intent == 'getWaste':
-        body = getWaste(SESSION_ID, original, waste, status)
-    elif intent == 'getStatus':
-        body = getWaste(SESSION_ID, original, waste, status)
-    else:
-        body = generateResponse("oooh what?")
+	parameters = body_event["queryResult"]["parameters"]
+	SESSION_ID = body_event["session"]
+	original = parameters["original"]
+	waste = parameters["waste"]
+	print(body_event["queryResult"]["outputContexts"])
+	print("waste: %s" % waste)
+	status = parameters["status"]
+	intent = body_event["queryResult"]["intent"]["displayName"]
+	print("intent %s" % intent)
 
-    print(body)
+	if intent == 'getWaste':
+		body = getWaste(SESSION_ID, original, waste, status)
+	elif intent == 'getStatus':
+		body = getStatus(SESSION_ID, original, waste, status)
+	else:
+		body = generateResponse(SESSION_ID, {'waste':''}, "oooh what?")
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+	print("response for server  %s" % body)
 
-    return response
+	response = {
+		"statusCode": 200,
+		"body": json.dumps(body)
+	}
+
+	return response
 
 
 def getWaste(SESSION_ID, original, waste, status):
-    output_params = {"waste": waste}
+	output_params = {"waste": waste}
 
-    file = open('waste.csv', 'r')
-    dataset = [line.rstrip('\n').split(',') for line in file]
-    file.close()
+	file = open('waste.csv', 'r')
+	dataset = [line.rstrip('\n').split(',') for line in file]
+	file.close()
 
-    wastes = []
+	wastes = []
 
-    for row in dataset:
-        if waste in [value.strip() for value in row[4].split('|')]:
-            wastes.append(row)
+	for row in dataset:
+		if waste in [value.strip() for value in row[4].split('|')]:
+			wastes.append(row)
 
-    if len(wastes) > 1:
-        message = "Do you mean " + ' or '.join('**' + value + '**' for value in wastes[3]) + "?"
-        response = generateResponse(SESSION_ID, output_params, message)
-    elif len(wastes) == 1:
+	print("waste array: %s" % wastes)
 
-        if status != '':
-            if status == 'clean':
-                response = generateResponse(SESSION_ID, output_params, wastes[0][1])
-            elif status == 'dirty':
-                response = generateResponse(SESSION_ID, output_params, wastes[0][2])
-            else:
-                response = generateResponse(SESSION_ID, output_params, "Is the " + waste + " clean or dirty?")
+	if len(wastes) > 1:
+		message = "Do you mean " + ' or '.join('**' + value + '**' for value in wastes[3]) + "?"
+		return generateResponse(SESSION_ID, output_params, message)
+	elif len(wastes) == 1:
 
-    return response
+		if status != '':
+			if status == 'clean':
+				return generateResponse(SESSION_ID, output_params, wastes[0][1])
+			elif status == 'dirty':
+				return generateResponse(SESSION_ID, output_params, wastes[0][2])
+			else:
+				return generateResponse(SESSION_ID, output_params, "Is the " + waste + " clean or dirty?")
+		else:
+			return generateResponse(SESSION_ID, output_params, "Is the " + waste + " clean or dirty?")
+			
+	else:
+		print("nessuno mi gestisce2")
+		return generateResponse(SESSION_ID, output_params, "Non trovato niente2")
+
+	return generateResponse(SESSION_ID, output_params, "Non trovato niente3")
 
 
-def getStatus(body_event): pass
+def getStatus(SESSION_ID, original, waste, status): 
+	return generateResponse(SESSION_ID, output_params, "Gestire lo status")
 
 
 def generateResponse(SESSION_ID, output_params, message):
-    return {
-        "payload": {
-            "google": {
-                "expectUserResponse": True,
-                "richResponse": {
-                    "items": [
-                        {
-                            "simpleResponse": {
-                                "textToSpeech": message
+	response =  {
+		"payload": {
+			"google": {
+				"expectUserResponse": True,
+				"richResponse": {
+					"items": [
+						{
+							"simpleResponse": {
+								"textToSpeech": message
 
-                            }
-                        }
-                    ]
-                }
-            }
-        },
-        "outputContexts": [
+							}
+						}
+					]
+				}
+			}
+		}
+	}
+	if output_params != None:
+		response["outputContexts"] = [
             {
-                "name": "projects/testrecycling/agent/sessions/"+SESSION_ID+"/contexts/context name",
+                "name": "projects/testrecycling/agent/sessions/"+SESSION_ID+"/contexts/context_name",
                 "lifespanCount": 5,
                 "parameters": {
                     output_params
                 }
             }
-        ],
-        "followupEventInput": {
-            "name": "event name",
-            "languageCode": "en-US",
-            "parameters": {
-                "param": "param value"
-            }
-        }
-    }
+        ]
+	
